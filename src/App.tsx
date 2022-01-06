f741378c75fd8e96721cc4f6ef08f99de7d1f053
Misha Poleshchenkov <mikhail.poleshchenkov@mail.ru>
2022-01-06 17:49:53 +0300

    added sorting

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,11 @@ import { TGame } from './@types/game';
 
 function App() {
 
+  // states
   const [calendar, setCalendar] = useState<TGame[] | []>([])
+  const [search, setSearch] = useState<string>('')
 
-
+  //
   const loadCalendar = async(offset: number = 0) => {
     await axios.get(`https://footballista.ru/api/seasons/5099/calendar_paginated?limit=10&offset=${offset}`)
       .then((res: any) => setCalendar((prev: TGame[] | []) => prev ? [...prev, ...res.data.items] : res.data.items))
@@ -26,17 +28,25 @@ function App() {
   }, [])
 
 
+  // add more game in calendar
   const addMore = async() => {
     loadCalendar(calendar.length)
   }
 
 
+  // filtredCalendar
+  const filtredCalendar: TGame[] | [] = calendar.filter((game: TGame) => {
+    return `${game.teamHome.name} ${game.teamAway.name}`.toLowerCase().includes(search.toLowerCase())
+  }) || calendar
+
+
+
   return (
     <div>
-      <Search />
+      <Search search={search} setSearch={setSearch}/>
       <div className="wrapper">
-        {calendar 
-          ? calendar.map((game: TGame) => <Card 
+        {filtredCalendar
+          ? filtredCalendar.map((game: TGame) => <Card 
                                           key={game._id} 
                                           id={game._id}
                                           tourNumber={game.tourNumber} 
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,19 @@
-const Search = () => {
+import { FC } from "react" 
+
+type SearchProps = {
+    search: string
+    setSearch: any
+}
+
+const Search: FC<SearchProps> = ({search, setSearch}) => {
     return (
         <div className="wrapperSearch">
             <div className="search">
-                <input type="text" placeholder="Поиск здесь..."/>
+                <input
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    type="text" 
+                    placeholder="Поиск здесь..."/>
                 <img height={20} width={20} src="https://i.imgur.com/L33v1KO.png" alt="search" />
             </div>
         </div>
